0863351dd179e1bcb265dfe40131d079249949b3
lsh000407 <lsh000407@hanmail.net>
2025-07-09 18:30:13 +0900

    git setting

--- main.py
+++ main.py
@@ -15,15 +15,19 @@ global empty
 # 주차장 인식
 lot_number = input("원하는 lot 번호를 입력하세요 (예: 1, 2): ")
 
+
 # 서버에 주차장 정보 요청
 module_path = f"GUI.lot_{lot_number}.navi"
+
 
 # GUI 출력 및 선호도 받기
 GUI_module = importlib.import_module(module_path)
 map_matrix = getattr(GUI_module, 'map_matrix')     # from GUI.lot_1.navi import map_matrix
 Navi = getattr(GUI_module, 'Navi')                 # from GUI.lot_1.navi import Navi
 
+
 # Thread run : 위치 추정 + path_planning + GUI 새로고침
+
 
 
 # GUI 내비게이션 출력
