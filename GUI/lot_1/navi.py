b13920cc166a0df18a30a6d7260a36da451ae404
LSH <lsh000407@hanmail.net>
2025-08-21 14:47:28 +0900

    naming

--- GUI/lot_1/navi.py
+++ GUI/lot_1/navi.py
@@ -1,64 +1,133 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
-from .ui_navi import Ui_MainWindow  # Qt Designer로 변환한 클래스 import
-from .ui_screen1 import Ui_MainWindow as Ui_Screen1  # Qt Designer로 변환한 클래스 import
-from .ui_screen2 import Ui_MainWindow as Ui_Screen2  # Qt Designer로 변환한 클래스 import
-from .ui_screen3 import Ui_MainWindow as Ui_Screen3  # Qt Designer로 변환한 클래스 import
-from .ui_screen4 import Ui_MainWindow as Ui_Screen4  # Qt Designer로 변환한 클래스 import
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
+from PySide6.QtGui import QPainter, QPen, QBrush
+from PySide6.QtCore import Qt, QPoint, QTimer
+from ui_navi import Ui_MainWindow  # pyside6-uic로 생성된 파일
+from typing import List, Tuple
+from map import Map
 
-map_matrix = [
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,0,1,2]
-    ]
+class DotOverlay(QWidget):
+    """stackedWidget 위에 덮는 투명 캔버스: points에 있는 좌표를 빨간 점으로 그림"""
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.points = []
+        # 마우스 이벤트 통과 & 배경 투명
+        self.setAttribute(Qt.WA_TransparentForMouseEvents, True)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
 
-class Navi(QMainWindow):
+    def set_points(self, pts):
+        # pts: [(x,y), ...]
+        self.points = [QPoint(int(x), int(y)) for (x, y) in pts]
+        self.update()
+
+    def clear(self):
+        self.points = []
+        self.update()
+
+    def paintEvent(self, _):
+        if not self.points:
+            return
+        p = QPainter(self)
+        p.setRenderHint(QPainter.Antialiasing, True)
+        p.setPen(QPen(Qt.red, 1))
+        p.setBrush(QBrush(Qt.red))
+        radius = 4  # 점 반경
+        for pt in self.points:
+            p.drawEllipse(pt, radius, radius)
+
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)  # UI 구성요소를 현재 창에 적용
-
-class Screen1(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Screen1()
         self.ui.setupUi(self)
+        self.ui.stackedWidget.setCurrentIndex(0)
+        self.ui.car.setVisible(False)
 
-    def next_screen(self):
+        # 현재 위치를 담을 좌표
+        self.pos_x = 0
+        self.pos_y = 0
+
+        # 찍을 경로 좌표
+        self.path = []
+
+        # stackedWidget 전체를 덮는 투명 오버레이
+        self.overlay = DotOverlay(self.ui.stackedWidget)
+        self.overlay.setGeometry(self.ui.stackedWidget.rect())
+        self.ui.stackedWidget.currentChanged.connect(self._sync_overlay_geometry)
+
+    def _sync_overlay_geometry(self, _index=None):
+        # 페이지 전환/리사이즈 시 오버레이 크기 동기화
+        self.overlay.setGeometry(self.ui.stackedWidget.rect())
+        self.overlay.raise_()  # 항상 최상단
+
+    def resizeEvent(self, e):
+        super().resizeEvent(e)
+        self._sync_overlay_geometry()
+
+    def terminate(self):
         self.close()
-        self.next_window = Screen2()
-        self.next_window.show()
+
+    def save_type(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+        self.overlay.clear()
+
+    def save_near(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+
+        # path에 담긴 모든 좌표에 빨간 점 출력
+        self._sync_overlay_geometry()
+        self.overlay.set_points(self.path)
+        self.overlay.show()
+
+        # 타이머를 사용하여 moving 함수를 주기적으로 호출
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.moving)
+        self.timer.start(50)  # 50ms마다 호출 (초당 20회)
+
+    def moving(self):
+        # 차 위치 옮기기 Test
+        self.pos_y -= 10
+        self.ui.car.move(self.pos_x, self.pos_y)
+        self.ui.car.setVisible(True)
+
+    def transform_points(self, points):
+        """
+        입력: [(x, y), ...] 형태의 좌표 리스트
+        처리: (x, y) -> (y, x)로 교환 후
+            x' = 10.43 * (y) + 29.68
+            y' = 11.78 * (x) + 53.98
+        출력: [(x', y'), ...]
+        """
+        return [(10.43 * y + 29.68, 11.78 * x + 53.98) for x, y in points]
 
 
-class Screen2(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Screen2()
-        self.ui.setupUi(self)
+if __name__ == "__main__":
 
-    def next_screen(self):
-        self.close()
-        self.next_window = Screen3()
-        self.next_window.show()
+    Parking_lot = Map()
+    Parking_lot.map_reset()
 
-class Screen3(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Screen3()
-        self.ui.setupUi(self)
+    app = QApplication(sys.argv)
+    navi = MainWindow()
 
-    def next_screen(self):
-        self.close()
-        self.next_window = Screen4()
-        self.next_window.show()
+    navi.pos_x = 200
+    navi.pos_y = 550
+    
+    path_prev = [
+        (58, 24), (57, 24), (56, 24), (55, 24), (54, 24),
+        (54, 25), (54, 26), (54, 27), (54, 28), (54, 29),
+        (54, 30), (54, 31), (54, 32), (54, 33), (54, 34),
+        (54, 35), (54, 36), (54, 37), (54, 38), (54, 39),
+        (54, 40), (54, 41), (54, 42), (54, 43), (54, 44),
+        (54, 45), (54, 46), (54, 47), (54, 48), (54, 49),
+        (54, 50), (54, 51), (54, 52), (54, 53), (54, 54),
+        (54, 55), (54, 56), (54, 57), (54, 58), (54, 59),
+        (54, 60), (54, 61), (54, 62), (54, 63), (54, 64),
+        (54, 65), (53, 65), (52, 65), (51, 65), (50, 65),
+        (49, 65), (48, 65)
+    ]
+    
+    navi.path = navi.transform_points(path_prev)
 
-class Screen4(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Screen4()
-        self.ui.setupUi(self)
-
-    def next_screen(self):
-        self.close()
-        self.next_window = Navi()
-        self.next_window.show()
+    navi.showFullScreen()  # 전체 화면
+    sys.exit(app.exec())
--- GUI/lot_1/ui_navi.py
+++ GUI/lot_1/ui_navi.py
@@ -1,42 +1,162 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'naviYlGdre.ui'
+## Form generated from reading UI file 'navireGmSJ.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.13
+## Created by: Qt User Interface Compiler version 6.9.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *  # type: ignore
-from PySide6.QtGui import *  # type: ignore
-from PySide6.QtWidgets import *  # type: ignore
-
-from . import resources_rc
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QStackedWidget, QWidget)
+import resources_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1437, 716)
+        MainWindow.resize(1280, 800)
+        MainWindow.setMinimumSize(QSize(1280, 800))
+        MainWindow.setAnimated(True)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.main_label = QLabel(self.centralwidget)
-        self.main_label.setObjectName(u"main_label")
-        self.main_label.setGeometry(QRect(0, 0, 1440, 718))
-        self.main_label.setStyleSheet(u"background-color : rgb(222, 255, 248)")
-        self.main_label.setPixmap(QPixmap(u":/parkinglot/prototype.jpg"))
-        self.main_label.setScaledContents(True)
-        self.main_label.setWordWrap(False)
+        self.centralwidget.setMinimumSize(QSize(1280, 800))
+        self.centralwidget.setMaximumSize(QSize(16777215, 16777215))
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.stackedWidget = QStackedWidget(self.centralwidget)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.stackedWidget.setMinimumSize(QSize(1280, 800))
+        self.stackedWidget.setStyleSheet(u"")
+        self.preference1 = QWidget()
+        self.preference1.setObjectName(u"preference1")
+        self.background1 = QLabel(self.preference1)
+        self.background1.setObjectName(u"background1")
+        self.background1.setGeometry(QRect(0, 0, 1280, 800))
+        self.background1.setMinimumSize(QSize(1280, 800))
+        self.background1.setPixmap(QPixmap(u":/preference/001.png"))
+        self.elec = QPushButton(self.preference1)
+        self.elec.setObjectName(u"elec")
+        self.elec.setGeometry(QRect(210, 300, 151, 151))
+        self.elec.setStyleSheet(u"background-color:rgb(214, 226, 232);")
+        icon = QIcon()
+        icon.addFile(u":/button/button_elec.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.elec.setIcon(icon)
+        self.elec.setIconSize(QSize(150, 150))
+        self.nomal = QPushButton(self.preference1)
+        self.nomal.setObjectName(u"nomal")
+        self.nomal.setGeometry(QRect(390, 300, 151, 151))
+        self.nomal.setStyleSheet(u"background-color:rgb(214, 226, 232);")
+        icon1 = QIcon()
+        icon1.addFile(u":/button/button_nomal.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.nomal.setIcon(icon1)
+        self.nomal.setIconSize(QSize(150, 150))
+        self.handicap = QPushButton(self.preference1)
+        self.handicap.setObjectName(u"handicap")
+        self.handicap.setGeometry(QRect(210, 490, 151, 151))
+        self.handicap.setStyleSheet(u"background-color:rgb(214, 226, 232);")
+        icon2 = QIcon()
+        icon2.addFile(u":/button/button_handicap.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.handicap.setIcon(icon2)
+        self.handicap.setIconSize(QSize(150, 150))
+        self.small = QPushButton(self.preference1)
+        self.small.setObjectName(u"small")
+        self.small.setGeometry(QRect(390, 490, 151, 151))
+        self.small.setStyleSheet(u"background-color:rgb(214, 226, 232);")
+        icon3 = QIcon()
+        icon3.addFile(u":/button/button_small.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.small.setIcon(icon3)
+        self.small.setIconSize(QSize(150, 150))
+        self.stackedWidget.addWidget(self.preference1)
+        self.preference2 = QWidget()
+        self.preference2.setObjectName(u"preference2")
+        self.background2 = QLabel(self.preference2)
+        self.background2.setObjectName(u"background2")
+        self.background2.setGeometry(QRect(0, 0, 1280, 800))
+        self.background2.setMinimumSize(QSize(1280, 800))
+        self.background2.setPixmap(QPixmap(u":/preference/002.png"))
+        self.Entrance = QPushButton(self.preference2)
+        self.Entrance.setObjectName(u"Entrance")
+        self.Entrance.setGeometry(QRect(200, 290, 391, 101))
+        font = QFont()
+        font.setPointSize(36)
+        self.Entrance.setFont(font)
+        self.Entrance.setStyleSheet(u"background-color: rgb(112, 177, 186);")
+        self.Exit = QPushButton(self.preference2)
+        self.Exit.setObjectName(u"Exit")
+        self.Exit.setGeometry(QRect(200, 410, 391, 101))
+        self.Exit.setFont(font)
+        self.Exit.setStyleSheet(u"background-color: rgb(112, 177, 186);")
+        self.Mall = QPushButton(self.preference2)
+        self.Mall.setObjectName(u"Mall")
+        self.Mall.setGeometry(QRect(200, 530, 391, 101))
+        self.Mall.setFont(font)
+        self.Mall.setStyleSheet(u"background-color: rgb(112, 177, 186);")
+        self.stackedWidget.addWidget(self.preference2)
+        self.navi = QWidget()
+        self.navi.setObjectName(u"navi")
+        self.navi.setMinimumSize(QSize(1280, 800))
+        self.navi.setStyleSheet(u"")
+        self.car = QLabel(self.navi)
+        self.car.setObjectName(u"car")
+        self.car.setGeometry(QRect(200, 540, 151, 131))
+        self.car.setPixmap(QPixmap(u":/car/car.png"))
+        self.car.setScaledContents(True)
+        self.map = QLabel(self.navi)
+        self.map.setObjectName(u"map")
+        self.map.setGeometry(QRect(0, 0, 1280, 800))
+        self.map.setMinimumSize(QSize(1280, 800))
+        self.map.setPixmap(QPixmap(u":/parkinglot/003.png"))
+        self.map.setScaledContents(False)
+        self.terminate = QPushButton(self.navi)
+        self.terminate.setObjectName(u"terminate")
+        self.terminate.setGeometry(QRect(1190, 740, 75, 41))
+        font1 = QFont()
+        font1.setFamilies([u"\ud734\uba3c\ubaa8\uc74cT"])
+        font1.setPointSize(10)
+        self.terminate.setFont(font1)
+        self.terminate.setStyleSheet(u"background-color:rgb(211, 0, 0);")
+        self.stackedWidget.addWidget(self.navi)
+        self.map.raise_()
+        self.car.raise_()
+        self.terminate.raise_()
+
+        self.gridLayout.addWidget(self.stackedWidget, 0, 0, 1, 1)
+
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
+        self.nomal.clicked.connect(MainWindow.save_type)
+        self.Entrance.clicked.connect(MainWindow.save_near)
+        self.terminate.clicked.connect(MainWindow.terminate)
+
+        self.stackedWidget.setCurrentIndex(1)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.main_label.setText("")
+        self.background1.setText("")
+        self.elec.setText("")
+        self.nomal.setText("")
+        self.handicap.setText("")
+        self.small.setText("")
+        self.background2.setText("")
+        self.Entrance.setText(QCoreApplication.translate("MainWindow", u"Entrance", None))
+        self.Exit.setText(QCoreApplication.translate("MainWindow", u"Exit", None))
+        self.Mall.setText(QCoreApplication.translate("MainWindow", u"Mall", None))
+        self.car.setText("")
+        self.map.setText("")
+        self.terminate.setText(QCoreApplication.translate("MainWindow", u"\uc885\ub8cc", None))
     # retranslateUi
 
