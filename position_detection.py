05fa686ee5f2a44382477fa9f6f5b509467c85ce
KimByungJoon <sgk012423@gmail.com>
2025-08-31 20:44:10 +0900

    Update position_detection.py

--- position_detection.py
+++ position_detection.py
@@ -174,4 +174,4 @@ def run_all_and_print_row_col(port="/dev/ttyS0", baud=115200, line_timeout=1.0):
     return row_col
 
 if __name__ == "__main__":
-    run_all_and_print_row_col(port="/dev/ttyS0", baud=115200, line_timeout=0.5)
+    run_all_and_print_row_col(port="/dev/ttyS0", baud=115200, line_timeout=1.0)
